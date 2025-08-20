4a904040090efe0b4f3981d069615a7e9856e89e
Magdaleno <magdaleno.garcia@fraternitas.io>
2025-08-20 05:42:34 -0600

    Test case 1

--- lambda/main.py
+++ lambda/main.py
@@ -2,7 +2,7 @@ def lambda_handler(event, context):
     try:
         return {
             "statusCode": 200,
-            "body": "hola mundo OmarJ",
+            "body": "hola mundo OmarJ, Test case 1",
         }
     except Exception as e:
         return {
